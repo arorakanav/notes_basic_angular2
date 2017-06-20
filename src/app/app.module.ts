3f4e2bd99285cb54cf5277e780a68d6e02727e2e
arorakanav11 <arorakanav11@gmail.com>
2017-06-20 10:25:15 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import { MaterialModule } from '@angular/material';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './container/container.component';
 import { AppBarComponent } from './app-bar/app-bar.component';
-
-
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
-import { MaterialModule } from '@angular/material';
 import { NoteCardComponent } from './note-card/note-card.component';
 import { NotesContainerComponent } from './notes-container/notes-container.component';
 import { NoteCreatorComponent } from './note-creator/note-creator.component';
+
 
 
 @NgModule({
